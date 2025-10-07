d0008a5d4065f0174ece02b26a450e27274777b0
Carlos Juanta <cjuantat@miumg.edu.gt>
2025-10-07 11:54:46 -0600

    Buenas practicas frontend

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// Importar Bootstrap
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Importar nuestros estilos personalizados (deben ir después de Bootstrap)
+
 import './assets/css/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
